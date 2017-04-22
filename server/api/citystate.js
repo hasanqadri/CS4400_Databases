179c0bfd765ceec1263811d498f0b0cd5818be07
Mark Clayton <mclayton34>
2017-04-22 01:58:11 -0400

    Finished endpoints, need testing

---
+++ server/api/citystate.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+var auth = require('../middlewares/authentication');
+var citystate = require('../models/city_state');
+
+router.post('/new', [
+    auth.admin,
+    function (req, res, next) {
+        let our_city = new citystate(req.body.city, req.body.state);
+        our_city.make(
+            function(res) {
+                res.status(200).end();
+            },
+            function(err) {
+                log.debug(err);
+                res.status(500).end();
+            }
+        )
+    }
+]);
+
+router.post('/list', [
+    auth.user,
+    function (req, res, next) {
+        citystate.fetch({order: req.body.order, like: req.body.like}, function (results) {
+            res.results = results;
+            next();
+        }, function (err) {
+            log.debug(err);
+            res.end();
+        });
+    },
+    function (req, res) {
+        res.json(res.results);
+        res.status(200).end();
+    }
+]);
+
+module.exports = router;
---
+++ server/api/poi.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+var auth = require('../middlewares/authentication');
+var poi = require('../models/poi');
+
+router.post('/new', [
+    auth.official,
+    function (req, res, next) {
+        let our_poi = new poi(req.body.location_name, req.body.city, null, null, req.body.state, req.body.zip);
+        our_poi.make(
+            function(res) {
+                res.status(200).end();
+            },
+            function(err) {
+                if (err.errno === 1062) {
+                    res.status(409).end();
+                } else {
+                    res.status(500).end();
+                }
+            }
+        )
+    }
+]);
+
+router.post('/list', [
+    auth.user
+    ,
+    function (req, res, next) {
+        poi.fetch({order: req.body.order, like: req.body.like}, function (results) {
+            res.results = results;
+            next();
+        }, function (err) {
+            log.debug(err);
+            res.end();
+        });
+    },
+    function (req, res) {
+        res.json(res.results);
+        res.status(200).end();
+    }
+]);
+
+module.exports = router;
