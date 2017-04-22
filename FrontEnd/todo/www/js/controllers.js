bacd38c17946a9fc90dfe0c8844b16a870c9c0f0
Mark Clayton <mclayton34@gatech.edu>
2017-04-22 01:39:53 -0400

    changed port & endpoint

--- FrontEnd/todo/www/js/controllers.js
+++ FrontEnd/todo/www/js/controllers.js
@@ -69,7 +69,7 @@ angular.module('starter')
     $scope.login = function() {
         console.log($scope.data.user);
         console.log($scope.data.pass);
-        var request = $.post("http://localhost:9000/", { user : $scope.data.user, pass : $scope.data.pass });
+        var request = $.post("http://localhost:3000/api/login", { username : $scope.data.user, password : $scope.data.pass });
  
         request.done(function( msg ) {
           console.log(msg);
