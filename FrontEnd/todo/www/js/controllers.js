91fe5415a70f47aeae8d361a5c35be5cadbf21c4
jzhang761 <joshzhang@gatech.edu>
2017-04-22 02:35:29 -0400

    yoodoo

--- FrontEnd/todo/www/js/controllers.js
+++ FrontEnd/todo/www/js/controllers.js
@@ -126,6 +126,13 @@ angular.module('starter')
     $rootScope.goBack = function() {
            $state.go('main.dash');
     };
+    var request = $.post("http://localhost:3000/api/poi/list", {});
+
+    request.done(function( msg ) {
+      $state.officials = msg;
+    }).fail(function( msg ) {
+        alert("Could not get user list");
+    });
 }])
 
 .controller('addDataCtrl', ['$state', '$scope','$rootScope', function($state, $scope, $rootScope) {
