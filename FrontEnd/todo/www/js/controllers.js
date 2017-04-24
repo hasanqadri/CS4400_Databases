dc3b2f7b7927fa2c45d2866d05922599e2c17435
ccai41 <ccai41@gatech.edu>
2017-04-24 04:20:41 -0400

    controllers

--- FrontEnd/todo/www/js/controllers.js
+++ FrontEnd/todo/www/js/controllers.js
@@ -395,16 +395,25 @@
 
 .controller('adminCtrl', ['$state', '$scope','$rootScope', function($state, $scope, $rootScope) {
     $scope.officials = [];
-    console.log(JSON.stringify({"vals":{"approved":null}}));
-    var request = $.post("http://" + host + "/api/users/list_officials", JSON.stringify({"vals":{"approved":null}}));
-    request.done(function( msg ) {
-      $scope.officials = msg;
-      for (i in $scope.officials) {
-        i.checked = false;
-      }
-    }).fail(function( msg ) {
-        alert("Could not get user list");
-    });
+    $.ajax({
+            type: "POST",
+            url: "http://" + host + "/api/users/list_officials",
+            data: JSON.stringify({"vals":{"approved":"pending"}}),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function(msg) {
+              $scope.officials = msg;
+              console.log($scope.officials);
+              for (i in $scope.officials) {
+                i.checked = false;
+              }
+
+            },
+            error: function(msg) {
+                console.log("Failed to get pending data.");
+                console.log(msg);
+            }
+      });
 
     $scope.submit = function(action) {
       var updateVal = (action == "Reject")? 0 : 1;
@@ -431,19 +440,6 @@
 
 .controller('adminPendingDataCtrl', ['$state', '$scope','$rootScope', function($state, $scope, $rootScope) {
     $scope.pendingData = [];
-<<<<<<< HEAD
-    var request = $.post("http://" + host + "/api/datapoint/list", JSON.stringify({"vals":{"accepted": "pending"}}));
-    request.done(function( msg ) {
-      $scope.pendingData = msg;
-      console.log($scope.pendingData);
-      for (i in $scope.pendingData) {
-        i.checked = false;
-      }
-    }).fail(function( msg ) {
-        console.log("Could not get pending data list");
-    })
-=======
-
 
      $.ajax({
             type: "POST",
@@ -464,7 +460,6 @@
                 console.log(msg);
             }
       });
->>>>>>> f70de04c4df787ea4a688833d7d7cfc5fc802692
 
     $scope.submit = function(action) {
       var updateVal = (action == "Reject")? "rejected" : "approved";
